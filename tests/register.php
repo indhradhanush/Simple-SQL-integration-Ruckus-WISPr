d6eed5218490449c1a3981d8f84e8c5af0015475
Karthikeyan Krish <indhradhanush@gmail.com>
2024-05-16 13:15:48 +0530

    Add files via upload

---
+++ tests/register.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CS Free-WiFi - Powered by Ruckus Networks</title>
+    <link rel="stylesheet" href="all.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" media="screen" rel="stylesheet" type="text/css" />
+
+<style>
+
+footer {
+  color: #fff;
+  border-top: 1px solid #000;
+  padding: 16px;
+  font-family: "Neue Helvetica W01", sans-serif;
+  font-weight: 300;
+  letter-spacing: 0.4px;
+  line-height: 1.2;
+  background-color: #2f3031;
+  background-repeat: repeat-x;
+  background-position: 0 0;
+  background-image: url("http://6915416c32e4851eca5d-c094c6710edd9b0999733b05a7cec13d.r9.cf2.rackcdn.com/images/layout/footer.png");
+  background-image: -webkit-gradient(linear, 50% 0%, 50% 100%, color-stop(0%, #161617), color-stop(3%, #2a2b2c), color-stop(16%, #2f3031), color-stop(100%, #2f3031));
+  background-image: url("data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4gPHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PGxpbmVhckdyYWRpZW50IGlkPSJncmFkIiBncmFkaWVudFVuaXRzPSJvYmplY3RCb3VuZGluZ0JveCIgeDE9IjAuNSIgeTE9IjAuMCIgeDI9IjAuNSIgeTI9IjEuMCI+PHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iIzE2MTYxNyIvPjxzdG9wIG9mZnNldD0iMyUiIHN0b3AtY29sb3I9IiMyYTJiMmMiLz48c3RvcCBvZmZzZXQ9IjE2JSIgc3RvcC1jb2xvcj0iIzJmMzAzMSIvPjxzdG9wIG9mZnNldD0iMTAwJSIgc3RvcC1jb2xvcj0iIzJmMzAzMSIvPjwvbGluZWFyR3JhZGllbnQ+PC9kZWZzPjxyZWN0IHg9IjAiIHk9IjAiIHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9InVybCgjZ3JhZCkiIC8+PC9zdmc+IA==");
+  background-size: 100%;
+  background-image: -webkit-gradient(linear, 50% 0%, 50% 100%, color-stop(0%, #161617), color-stop(3%, #2a2b2c), color-stop(16%, #2f3031), color-stop(100%, #2f3031));
+  background-image: -moz-linear-gradient(#161617 0%, #2a2b2c 3%, #2f3031 16%, #2f3031 100%);
+  background-image: -webkit-linear-gradient(#161617 0%, #2a2b2c 3%, #2f3031 16%, #2f3031 100%);
+  background-image: linear-gradient(#161617 0%, #2a2b2c 3%, #2f3031 16%, #2f3031 100%);
+}
+
+
+footer ul, footer p {
+  font-size: 12px;
+}
+
+footer ul {
+  padding-left: 0px;
+}
+
+footer li {
+  margin-bottom: 0px;
+}
+
+footer a:link, footer a:visited {
+  color: #fff;
+}
+
+
+footer .social {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  align-items: flex-start;
+  max-width: 320px;
+  margin: 0 auto;
+}
+
+footer .social a {
+  padding-right: 10px;
+  text-decoration: none;
+}
+
+footer .social a:before {
+  font-family: "Ruckus_Icons";
+  font-size: 32px;
+}
+
+footer .social a span {
+  display: none;
+}
+
+footer .social #blog a:before {
+  content: '\e00a';
+}
+
+footer .social #blog a:hover, footer .social #blog a:focus, footer .social #blog a:active {
+  color: #ea7600;
+  img: #ea7600;
+}
+
+footer .social #facebook a:before {
+  content: '\e00c';
+}
+
+footer .social #facebook a:hover, footer .social #facebook a:focus, footer .social #facebook a:active {
+  color: #3b5998;
+}
+
+footer .social #twitter a:before {
+  content: '\e001';
+}
+
+footer .social #twitter a:hover, footer .social #twitter a:focus, footer .social #twitter a:active {
+  color: #1da1f2;
+}
+
+footer .social #youtube a:before {
+  content: '\e000';
+}
+
+footer .social #youtube a:hover, footer .social #youtube a:focus, footer .social #youtube a:active {
+  color: #e62117;
+}
+
+footer .social #linkedin a:before {
+  content: '\e008';
+}
+
+footer .social #linkedin a:hover, footer .social #linkedin a:focus, footer .social #linkedin a:active {
+  color: #0077b5;
+}
+
+footer .social #instagram a:before {
+  content: '\e00b';
+}
+
+footer .social #instagram a:hover, footer .social #instagram a:focus, footer .social #instagram a:active {
+  color: #a3a3a3;
+}
+</style>
+
+
+    <style>
+      html, body {
+      min-height: 100%;
+      }
+      body, div, form, input, select, p { 
+      padding: 0;
+      margin: 0;
+      outline: none;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 16px;
+      color: #eee;
+      }
+      body {
+      background: url("bg.jpeg") no-repeat center;
+      background-size: cover;
+      }
+      h1, h2 {
+      text-transform: uppercase;
+      font-weight: 400;
+      }
+      h2 {
+      margin: 0 0 0 8px;
+      }
+      .main-block {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      padding: 25px;
+      background: rgba(0, 0, 0, 0.5); 
+      }
+      .left-part, form {
+      padding: 25px;
+      }
+      .left-part {
+      text-align: center;
+      }
+      .fa-graduation-cap {
+      font-size: 72px;
+      }
+      form {
+      background: rgba(0, 0, 0, 0.7); 
+      }
+      .title {
+      display: flex;
+      align-items: center;
+      margin-bottom: 20px;
+      }
+      .info {
+      display: flex;
+      flex-direction: column;
+      }
+      input, select {
+      padding: 5px;
+      margin-bottom: 30px;
+      background: transparent;
+      border: none;
+      border-bottom: 1px solid #eee;
+      }
+      input::placeholder {
+      color: #eee;
+      }
+      option:focus {
+      border: none;
+      }
+      option {
+      background: black; 
+      border: none;
+      }
+      .checkbox input {
+      margin: 0 10px 0 0;
+      vertical-align: middle;
+      }
+      .checkbox a {
+      color: #26a9e0;
+      }
+      .checkbox a:hover {
+      color: #85d6de;
+      }
+      .btn-item, button {
+      padding: 10px 5px;
+      margin-top: 20px;
+      border-radius: 5px; 
+      border: none;
+      background: #26a9e0; 
+      text-decoration: none;
+      font-size: 15px;
+      font-weight: 400;
+      color: #fff;
+      }
+      .btn-item {
+      display: inline-block;
+      margin: 20px 5px 0;
+      }
+      button {
+      width: 100%;
+      }
+      button:hover, .btn-item:hover {
+      background: #85d6de;
+      }
+      @media (min-width: 568px) {
+      html, body {
+      height: 100%;
+      }
+      .main-block {
+      flex-direction: row;
+      height: calc(100% - 50px);
+      }
+      .left-part, form {
+      flex: 1;
+      height: auto;
+      }
+      }
+    </style>
+  </head>
+
+
+<?PHP
+// select acctterminatecause from radacct where callingstationid='32-E3-FC-CE-7F-55' and acctstoptime regexp '2021-01-02 *' order by radacctid desc limit 1;
+
+$sip=$_GET['sip'];
+
+
+$callingstationid=strtoupper($_GET['client_mac']);
+$callingstationid = implode("-", str_split(str_replace(".", "", $callingstationid), 2));
+$curr_date=date("Y-m-d");
+$query="select acctterminatecause from radacct where callingstationid='".$callingstationid."' and acctstoptime regexp '".$curr_date." *' order by radacctid desc limit 1;";
+//echo $query;
+$servername = "localhost";
+$username = "radius";
+$password = "RadRuckus123";
+$dbname = "radius";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+$sesstimeout=$row["acctterminatecause"];
+
+
+?>
+
+  <body>
+    <div class="main-block">
+
+
+<?PHP
+if ($sesstimeout=="Session-Timeout")
+{
+?>
+      <div class="left-part">
+        <!--<img height=100 width=100 src="https://cdn1.iconfinder.com/data/icons/free-ui-1/24/Wifi-ui-ux-mobile-web-256.png">-->
+        <img height=100 width=100 src="JHKiP5e.jpg">
+        <h1>Your Daily Quota limit exceeded</h1>
+        <p>See you tomorrow</p>
+        <p>Session limit - 1 hour. Maximum sessions per day - 2.</p>
+        <div class="btn-group">
+          <a class="btn-item" href="https://training.ruckuswireless.com">Learn about Wi-Fi - Training by Ruckus</a>
+          <a class="btn-item" href="https://www.youtube.com/channel/UCpjMmx0SYp28_LpM9d2Yf3g">Networking Videos - Ruckus Education</a>
+        </div>
+      </div>
+
+
+<?PHP
+}
+else
+{
+?>
+      <div class="left-part">
+        <!--<img height=100 width=100 src="https://cdn1.iconfinder.com/data/icons/free-ui-1/24/Wifi-ui-ux-mobile-web-256.png">-->
+	<img height=100 width=100 src="JHKiP5e.jpg">
+        <h1>Register for Wi-Fi access</h1>
+        <p>Register with Roll number and Date of Birth. Followed by SMS verification by OTP received in pre-registered mobile number.</p>
+        <p>Session limit - 1 hour. Maximum sessions per day - 2.</p>
+	<div class="btn-group">
+          <a class="btn-item" href="https://training.ruckuswireless.com">Learn about Wi-Fi - Training by Ruckus</a>
+          <a class="btn-item" href="https://www.youtube.com/channel/UCpjMmx0SYp28_LpM9d2Yf3g">Networking Videos - Ruckus Education</a>
+        </div>
+      </div>
+<?PHP
+echo "<form method=post action='http://".$sip.":9997/login'>"
+?> 
+        <div class="title">
+	<img height=50 width=50 src="pencil.png">
+          <h2>Register here</h2>
+        </div>
+        <div class="info">
+
+  <input type="hidden" name="username" value="karthik"><br>
+  <input type="hidden" name="password" value="karthik123">
+
+          <input class="fname" type="text" name="rollno" placeholder="Roll Number">
+          <input type="date" name="dob" placeholder="Date of Birth">
+        </div>
+        <div class="checkbox">
+          <input type="checkbox" name="checkbox"><span>I agree to the <a href="privacy-policy.html">Privacy Policy for Ruckus Free Wi-Fi</a></span>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+<?PHP
+}
+$conn->close();
+
+?>
+</div>
+<footer>
+<div align=center>
+<p>Powered by <a href="http://www.ruckuswireless.com/">Ruckus Networks</a></p>
+<p>A CommScope Company, 350 West Java Dr. Sunnyvale, CA 94089 USA</p>
+  <ul class="social">
+    <li id="blog"><a href="https://theruckusroom.ruckuswireless.com"><span>Blog</span></a></li>
+    <li id="facebook"><a href="https://www.facebook.com/ruckusnetworks"><span>Facebook</span></a></li>
+    <li id="twitter"><a href="https://twitter.com/ruckusnetworks"><span>Twitter</span></a></li>
+    <li id="youtube"><a href="https://www.youtube.com/user/ruckuswireless"><span>YouTube</span></a></li>
+    <li id="linkedin"><a href="https://www.linkedin.com/company/ruckus-networks/"><span>LinkedIn</span></a></li>
+    <li id="instagram"><a href="https://www.instagram.com/commscopetv/"><span>Instagram</span></a></li>
+  </ul>
+</div>
+</div>
+</footer>
+  </body>
+</html>
